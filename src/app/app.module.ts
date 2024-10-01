2f6f156604280df963541ea42a64fc40d57ea749
Omdevsinh-Zala <omdevsinh@krishaweb.com>
2024-10-01 16:26:43 +0530

    Add env files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,7 +19,7 @@ import { MainRouterComponent } from './main-router/main-router.component';
 import { LoginComponent } from './form/login/login.component';
 import { SignupComponent } from './form/signup/signup.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { environment } from '../environments/environment.development';
+import { environment } from '../environments/environment';
 import { FirebaseAppModule, initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
@@ -27,10 +27,8 @@ import { getDatabase, provideDatabase } from '@angular/fire/database';
 import { MatError, MatFormField, MatLabel } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
 import { UpdateComponent } from './form/update/update.component';
-import { HomeComponent } from './main-pages/home/home.component';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatSelectModule } from '@angular/material/select';
-import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { ResetComponent } from './form/reset/reset.component';
 
 @NgModule({
